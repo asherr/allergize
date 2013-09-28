417262ca41fa915c161c6613a1f58d9d4d4adc90
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-09-28 15:29:08 -0400

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Allergize::Application.config.secret_token = '18ca66ed5933cf0e4a7a6a72be06e418cb24425bd45975431553f2a2e06eddba3e02f46eec40d975f46fb8fc0932dbd7c754fba48adc10d2af287654441a1af8'
