cadd84e48f4d432edf62f46e3d2d9ec3e83a90ba
Aaron Sherrin <aaron.sherrin@gmail.com>
2013-09-29 01:43:53 -0400

    Add env tokens for heroku

--- config/initializers/tokens.rb
+++ config/initializers/tokens.rb
@@ -1,4 +1,7 @@
 tokens_file = File.join("#{Rails.root}/config", "tokens.yml")
 if File.readable?(tokens_file)
   TOKENS = YAML.load_file(tokens_file)[Rails.env]
+else
+  binding.pry
+  TOKENS = { "foursquare" => { "client_id" => ENV["CLIENT_ID"], "client_secret" => ENV["SECRET_ID"] } } 
 end
