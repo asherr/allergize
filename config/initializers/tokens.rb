acd93e99900ecc05749b6651461ad235c788258a
Aaron Sherrin <aaron.sherrin@gmail.com>
2013-09-29 01:49:44 -0400

    DIE PRY DIE

--- config/initializers/tokens.rb
+++ config/initializers/tokens.rb
@@ -2,6 +2,5 @@ tokens_file = File.join("#{Rails.root}/config", "tokens.yml")
 if File.readable?(tokens_file)
   TOKENS = YAML.load_file(tokens_file)[Rails.env]
 else
-  binding.pry
   TOKENS = { "foursquare" => { "client_id" => ENV["CLIENT_ID"], "client_secret" => ENV["SECRET_ID"] } } 
 end
