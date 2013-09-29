b3efb6839f8ed9d9d3e45d06a041f18e6ddfad2b
Jordan Goldstein <mrjordangoldstein@gmail.com>
2013-09-29 09:37:58 -0400

    Fix name of token environment variable for Heroku

--- config/initializers/tokens.rb
+++ config/initializers/tokens.rb
@@ -2,5 +2,5 @@ tokens_file = File.join("#{Rails.root}/config", "tokens.yml")
 if File.readable?(tokens_file)
   TOKENS = YAML.load_file(tokens_file)[Rails.env]
 else
-  TOKENS = { "foursquare" => { "client_id" => ENV["CLIENT_ID"], "client_secret" => ENV["SECRET_ID"] } } 
+  TOKENS = { "foursquare" => { "client_id" => ENV["CLIENT_ID"], "client_secret" => ENV["CLIENT_SECRET"] } } 
 end
